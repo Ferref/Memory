a0b6814cde4022c029cd559668801626338908cb
Kovács Ferenc <kovacsferenc026@gmail.com>
2024-04-25 13:29:15 +0200

    jatekmod kivalasztas
    jatekmod kivalasztasanal sotet hatter a kivalasztott opcioknak soronkent

--- jatekosKapcsol.js
+++ jatekosKapcsol.js
@@ -1,2 +1,37 @@
 // Játékosmód => Játék
-// Ez fohja összekapcsolni őket
+// Ez fogja összekapcsolni őket
+
+const tablameretGombok = document.querySelectorAll('#tablameret .navGomb');
+const idozitoGombok = document.querySelectorAll('#idozito .navGomb');
+const nehezsegGombok = document.querySelectorAll('#nehezseg .navGomb');
+
+tablameretGombok.forEach(gomb => {
+  gomb.addEventListener('click', function () {
+    tablameretGombok.forEach(gomb => {
+      gomb.classList.remove('kivalasztott');
+    });
+    this.classList.add('kivalasztott');
+  });
+});
+
+idozitoGombok.forEach(gomb => {
+  gomb.addEventListener('click', function () {
+    idozitoGombok.forEach(gomb => {
+      gomb.classList.remove('kivalasztott');
+    });
+    this.classList.add('kivalasztott');
+  });
+});
+
+nehezsegGombok.forEach(gomb => {
+  gomb.addEventListener('click', function () {
+    nehezsegGombok.forEach(gomb => {
+      gomb.classList.remove('kivalasztott');
+    });
+    this.classList.add('kivalasztott');
+  });
+});
+
+
+
+
