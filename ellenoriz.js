df6da18b23d6736d147c05f3faaaf990deec059e
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-05-09 02:51:13 +0200

    working solo

--- ellenoriz.js
+++ ellenoriz.js
@@ -1,16 +1,17 @@
-var buttons = document.querySelectorAll('a[href^="#"] > button.navGomb');
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.navGomb');
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        var href = button.parentElement.getAttribute('href');
-        
-        if(confirm("Biztos ki akarsz lépni? A játéknak még nincsen vége.")) {
-            // User confirmed, do not perform any redirection
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // Az alapértelmezett esemény megakadályozása
+            
+            var href = button.parentElement.getAttribute('href');
+            
+            if (!confirm("Biztos ki akarsz lépni? A játéknak még nincsen vége.")) {
+                return; // Ha a felhasználó a "Mégsem" opciót választotta, egyszerűen térjünk vissza az eseménykezelőből
+            }
 
-        } else {
-            return;
-        }
+            window.location.href = href; // Ha a felhasználó megerősítette a kilépést, akkor irányítson át az adott oldalra
+        });
     });
 });
